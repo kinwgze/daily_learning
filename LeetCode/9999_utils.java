0f085c64052723ce1e4942c0616c5990a4a571df
kinwgze <wz297443228@outlook.com>
2022-01-20 15:56:21 +0800

    2022/01/20 update

--- LeetCode/9999_utils.java
+++ LeetCode/9999_utils.java
@@ -2,30 +2,32 @@ package leetcode;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Scanner;
 
+/**
+ * @author kinwgze
+ */
 public class ForGitHub {
+    /**
+     * 中文匹配正则表达式
+     */
+    static final String REGEX_CHINESE = "[\u4e00-\u9fa5]";
+
     public static void main(String[] args) {
 
-        String s = null;
+        String s = "0058.Length of Last Word 最后一个单词的长度";
         String res;
 
-        // 接受输入
-        Scanner sc = new Scanner(System.in);
-        if (sc.hasNextLine()) {
-            s = sc.nextLine();
-        }
-
         // md标题处理及输出
-        res = s.replace(" ", "_");
-        res = res.replace(".", "_");
-        res = res +".md";
-        System.out.println("\n" + "Markdown文件标题：" + res + "\n");
+
+        res = s.replaceAll(REGEX_CHINESE, "").replace(" ", "_")
+                .replace(".", "_") + ".md";
+        System.out.println("\n" + "Markdown文件标题：" + res.replace("_.", ".") + "\n");
 
         // 时间及commit信息处理及输出
         Date now = new Date();
         SimpleDateFormat  time = new SimpleDateFormat("yyyy/MM/dd");
         String titleTime = time.format(now);
+        String[] strs = s.split(" ");
         String[] strs2 = s.split("\\.");
         System.out.println("commit信息：" + titleTime + " " + strs2[0] + "." + strs[strs.length - 1] + "\n ");
 
